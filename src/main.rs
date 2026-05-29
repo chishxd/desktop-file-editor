3c1eec343e8f4274fc8d12ee0da5aa734d9d27a0
Chinmay Shet <chishxd@tutamail.com>
2026-05-29 14:13:26 +0530

    add Search functionalities
    sorry for a big commit, I just did not want some broken code in my commit history hehe

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use crossterm::event::{self, KeyCode};
 use ratatui::{
     Frame,
     layout::{Constraint, Layout, Rect},
-    style::{Color, Modifier, Style, Stylize},
-    text::{Line, Span},
+    style::{Color, Modifier, Style},
+    text::Line,
     widgets::{Block, List, ListState, Paragraph},
 };
 use ratatui_textarea::{CursorMove, Input, TextArea};
@@ -37,6 +37,9 @@ struct ParsedDesktopFile {
 
 enum AppMode {
     Browse,
+    Search {
+        input: TextArea<'static>,
+    },
     Edit {
         file_idx: usize,
         active_field: usize,
@@ -52,6 +55,7 @@ struct App {
     status_style: Style,
     status_created: Option<std::time::Instant>,
     status_job: Option<std::sync::mpsc::Receiver<DbUpdateResult>>,
+    displaying_indices: Vec<usize>,
 }
 
 fn main() -> std::io::Result<()> {
@@ -61,6 +65,7 @@ fn main() -> std::io::Result<()> {
 }
 
 impl App {
+    // The constructor for App's State
     fn new() -> std::io::Result<Self> {
         let mut app = Self {
             items: load_files()?,
@@ -71,12 +76,37 @@ impl App {
             status_message: None,
             status_style: Style::default(),
             status_job: None,
+            displaying_indices: (0..0).collect(),
         };
 
+        app.displaying_indices = (0..app.items.len()).collect();
         app.parse_current();
         Ok(app)
     }
 
+    // This function updates the indices to be displayed based on query
+    fn update_displaying_indices(&mut self, query: &str) {
+        let q = query.to_lowercase();
+        self.displaying_indices = self
+            .items
+            .iter()
+            .enumerate()
+            .filter_map(|(i, f)| {
+                if f.name.to_lowercase().contains(&q) {
+                    Some(i)
+                } else {
+                    None
+                }
+            })
+            .collect();
+    }
+    fn reset_search(&mut self) {
+        self.displaying_indices = (0..self.items.len()).collect();
+        self.idx = 0;
+        self.parse_current();
+    }
+
+    // The main run function
     fn run(mut self, list_state: &mut ListState) -> std::io::Result<()> {
         ratatui::run(|terminal| {
             loop {
@@ -88,9 +118,23 @@ impl App {
                     if let Some(key) = event::read()?.as_key_press_event() {
                         match &mut self.mode {
                             AppMode::Browse => match key.code {
+                                KeyCode::Esc => {
+                                    self.reset_search();
+                                }
+                                KeyCode::Char('/') => {
+                                    let mut input = TextArea::from(vec![String::new()]);
+                                    input.set_cursor_line_style(Style::default());
+                                    input.set_cursor_style(
+                                        Style::default().add_modifier(Modifier::REVERSED),
+                                    );
+                                    input.move_cursor(CursorMove::End);
+
+                                    self.mode = AppMode::Search { input };
+                                }
                                 KeyCode::Char('j') | KeyCode::Down => {
                                     if !self.items.is_empty() {
-                                        self.idx = (self.idx + 1).min(self.items.len() - 1);
+                                        self.idx =
+                                            (self.idx + 1).min(self.displaying_indices.len() - 1);
                                         self.parse_current();
                                     }
                                 }
@@ -98,7 +142,7 @@ impl App {
                                     self.idx = self.idx.saturating_sub(1);
                                     self.parse_current();
                                 }
-                                KeyCode::Char('q') | KeyCode::Esc => break Ok(()),
+                                KeyCode::Char('q') => break Ok(()),
                                 KeyCode::Enter => {
                                     if let Some(parsed_file) = &self.parsed_file {
                                         let mut name =
@@ -124,6 +168,57 @@ impl App {
                                     }
                                 }
                                 _ => {}
+                            },
+                            AppMode::Search { input } => match key.code {
+                                // TODO: Complete all inputs... Me need some thinking for what keys me needs, just placeholders for now
+                                KeyCode::Esc => {
+                                    self.mode = AppMode::Browse;
+                                    self.reset_search();
+                                }
+                                KeyCode::Char('j') | KeyCode::Down => {
+                                    if !self.items.is_empty() {
+                                        self.idx =
+                                            (self.idx + 1).min(self.displaying_indices.len() - 1);
+                                        self.parse_current();
+                                    }
+                                }
+                                KeyCode::Char('k') | KeyCode::Up => {
+                                    self.idx = self.idx.saturating_sub(1);
+                                    self.parse_current();
+                                }
+                                KeyCode::Enter => {
+                                    if let Some(parsed_file) = &self.parsed_file {
+                                        let mut name =
+                                            TextArea::from(vec![parsed_file.name.clone()]);
+                                        let mut exec =
+                                            TextArea::from(vec![parsed_file.exec.clone()]);
+                                        let mut icon =
+                                            TextArea::from(vec![parsed_file.icon.clone()]);
+
+                                        for ta in [&mut name, &mut exec, &mut icon] {
+                                            ta.set_cursor_line_style(Style::default());
+                                            ta.set_cursor_style(
+                                                Style::default().add_modifier(Modifier::REVERSED),
+                                            );
+                                            ta.move_cursor(CursorMove::End)
+                                        }
+
+                                        self.mode = AppMode::Edit {
+                                            file_idx: self.idx,
+                                            active_field: 0,
+                                            textareas: vec![name, exec, icon],
+                                        }
+                                    }
+                                }
+                                _ => {
+                                    let ta_input: Input = key.into();
+                                    input.input(ta_input);
+
+                                    let query = input.lines().first().cloned().unwrap_or_default();
+                                    self.update_displaying_indices(&query);
+                                    self.idx = 0;
+                                    self.parse_current();
+                                }
                             },
                             AppMode::Edit {
                                 file_idx,
@@ -163,7 +258,6 @@ impl App {
                                     let rx = spawn_update_desktop_database();
                                     self.status_job = Some(rx);
                                     self.status_style = Style::default().fg(Color::Green);
-                                    // self.status_created = Some(Instant::now());
                                 }
                                 _ => {
                                     let input: Input = key.into();
@@ -177,6 +271,7 @@ impl App {
         })
     }
 
+    // Clears Status Message after 2 seconds from footer
     fn clear_status_msg(&mut self) {
         if self.status_job.is_some() {
             return;
@@ -191,6 +286,9 @@ impl App {
         }
     }
 
+    // Argghh I am still learning async, forgive me if this code is terrible,
+    // So this function is polling for stattus returned after running the binary in a background thread and
+    // sets a status message based on output from the thread
     fn poll_status_job(&mut self) {
         if let Some(rx) = &self.status_job {
             use std::sync::mpsc::TryRecvError;
@@ -233,13 +331,17 @@ impl App {
     }
 
     fn parse_current(&mut self) {
-        self.parsed_file = self
-            .items
-            .get(self.idx)
-            .and_then(|f| parse_file(&f.path).ok());
+        if let Some(&real_idx) = self.displaying_indices.get(self.idx) {
+            self.parsed_file = self
+                .items
+                .get(real_idx)
+                .and_then(|f| parse_file(&f.path).ok());
+        } else {
+            self.parsed_file = None
+        }
     }
     fn render(&self, frame: &mut Frame, list_state: &mut ListState) {
-        let vertical = Layout::vertical([Constraint::Length(1), Constraint::Min(0)]).spacing(1);
+        let vertical = Layout::vertical([Constraint::Length(3), Constraint::Min(0)]).spacing(1);
         let [title_area, body_area] = frame.area().layout(&vertical);
 
         let cols = Layout::horizontal([Constraint::Percentage(40), Constraint::Percentage(60)])
@@ -248,23 +350,42 @@ impl App {
         let left = cols[0];
         let right = cols[1];
 
-        let title = Line::from_iter([
-            Span::from("Desktop File Editor").bold(),
-            Span::from("  q: quit  up/down: move  enter: edit"),
-        ]);
-        frame.render_widget(title.centered(), title_area);
+        match &self.mode {
+            AppMode::Search { input: query } => {
+                let mut box_view = query.clone();
+                box_view.set_block(Block::bordered().title("Search"));
+                frame.render_widget(&box_view, title_area);
+            }
 
-        let names: Vec<String> = self.items.iter().map(|f| f.name.clone()).collect();
+            _ => {
+                let text: Vec<Line<'static>> = vec!["  q: quit  /: search  enter: edit".into()];
+                let title = Paragraph::new(text).block(
+                    ratatui::widgets::Block::default()
+                        .title("Desktop File Editor")
+                        .borders(ratatui::widgets::Borders::ALL),
+                );
+                frame.render_widget(title.centered(), title_area);
+            }
+        }
+
+        let names: Vec<String> = self
+            .displaying_indices
+            .iter()
+            .map(|&i| self.items[i].name.clone())
+            .collect();
         list_state.select(Some(self.idx));
         self.render_list(names, frame, left, list_state);
 
         match self.mode {
             AppMode::Browse => self.render_details(frame, right),
+            AppMode::Search { .. } => self.render_details(frame, right),
             AppMode::Edit { .. } => self.render_editor(frame, right),
         }
 
         let footer_area = Layout::vertical([
             Constraint::Min(0),    // existing body
+            Constraint::Length(1), // status line
+            Constraint::Length(1), // status line
             Constraint::Length(1), // status line
         ])
         .split(frame.area())[1]; // get the bottom 1-line area
